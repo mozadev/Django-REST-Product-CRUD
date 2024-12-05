4e258d1e1eab1a542e57c1936b175110cde9106a
mozac <cesarmozareyes@gmail.com>
2024-12-05 18:11:47 -0500

    validate_categoria_id

--- product/serializers.py
+++ product/serializers.py
@@ -17,17 +17,28 @@ class ProductoSerializer(serializers.Serializer):
     descripcion = serializers.CharField()
     precio = serializers.DecimalField(max_digits=10, decimal_places=2)
     stock = serializers.IntegerField()
-    categoria_id = serializers.PrimaryKeyRelatedField(queryset=Categoria.objects.all(), source="categoria")
+    #categoria_id = serializers.PrimaryKeyRelatedField(queryset=Categoria.objects.all(), source="categoria")
+    categoria_id =serializers.IntegerField()
     categoria = serializers.SerializerMethodField()
 
     def get_categoria(self, obj):
         if obj.categoria is not None:
             return {
                 # "id": obj.categoria.id,
-                "nombre": obj.categoria.nombre,
-                "activo": obj.categoria.activo
+                "nombre": obj.categoria.nombre.upper(),
+                "activo": "si" if obj.categoria.activo else "No"
             }
         return None
+    
+    def validate_categoria_id(self, value):
+        if not Categoria.objects.filter(id=value, activo=True).exists():
+            raise serializers.ValidationError("La categoria proporcionada no existe.")
+        return value
+    
+    def validate_precio(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("El error debe ser mayor que 0")
+        return value
 
     def create(self, validated_data):
         return Producto.objects.create(**validated_data)
@@ -43,5 +54,8 @@ class ProductoSerializer(serializers.Serializer):
     
     
     
+
+
+
 
 
